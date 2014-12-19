3e870171d11b69fd45344f31e8870624a5c95449
Patrick M. Niedzielski <patrick@pniedzielski.net>
2014-12-19 20:04:45 +0000

    Add a bash_aliases file.

---
+++ .bash_aliases
@@ -0,0 +1,3 @@
+alias em="emacsclient --alternate-editor='' -n$@"
+alias emtty="emacsclient --alternate-editor='' -t $@"
+alias emnew="emacsclient --alternate-editor='' -c -n $@"
--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,7 @@ ln -srb .inputrc ~/
 ln -srb .gitconfig ~/
 ln -srb .gitmessage ~/
 ln -srb .bashrc ~/
+ln -srb .bash_aliases ~/
 ln -srb .emacs.d ~/
 
 # Bootstrap the Emacs configuration
