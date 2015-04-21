34e22b28e144b89738f795e6406b3a7f230ae2c3
Patrick M. Niedzielski <patrick@pniedzielski.net>
2015-04-21 21:12:28 +0000

    Change org-mode faces.

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -12,4 +12,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(org-level-1 ((t (:foreground "dodger blue" :weight bold :height 1.0))))
+ '(org-level-2 ((t (:foreground "#edd400" :weight bold :height 1.0)))))
