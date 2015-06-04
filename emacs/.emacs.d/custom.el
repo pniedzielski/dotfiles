4d4bd68f84acd2168263577763ff5a4acfda23e8
Patrick M. Niedzielski <patrick@pniedzielski.net>
2015-06-04 05:50:28 +0000

    [emacs] Configure `cperl-mode`.
    This was copied from my previous emacs configuration and adapted to
    `req-package`.  There are a few customizations to make the indentation
    more like the standard Perl conventions.  We also set
    `cperl-invalid-face` to `nil` to prevent annoying spaces at the ends of
    lines from being displayed.  When I want those, I use `whitespace-mode`.
    
    In the future I'd like to integrate `perlcritic` and `perltidy` into
    this as well.

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -5,12 +5,13 @@
  ;; If there is more than one, they won't work right.
  '(safe-local-variable-values
    (quote
-    ((ispell-dictionary . english)
-     (ispell-dictionary . "english")))))
+    ((ispell-dictionary . "english")))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(org-level-1 ((t (:foreground "dodger blue" :weight bold :height 1.0))))
- '(org-level-2 ((t (:foreground "#edd400" :weight bold :height 1.0)))))
+ '(org-level-2 ((t (:foreground "#edd400" :weight bold :height 1.0))))
+ '(cperl-array-face ((t (:inherit font-lock-variable-name-face :weight bold))))
+ '(cperl-hash-face ((t (:inherit font-lock-variable-name-face :slant italic)))))
